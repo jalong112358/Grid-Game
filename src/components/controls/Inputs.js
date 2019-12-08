1bc12d7fdfe982d399476604ae12fd9b7757ae29
Jake <jake.long112358@gmail.com>
2019-12-08 14:10:08 -0600

    initial commit

--- src/components/controls/Inputs.js
+++ src/components/controls/Inputs.js
@@ -11,22 +11,22 @@ export default function Inputs() {
           <div>
             <div id="inputs">
               <div className="arrow" onClick={() => updateDirections(0)}>
-                <svg height="36" width="36" fill="#434343">
+                <svg height="36" width="36" fill="rgb(24, 24, 24)">
                   <path d="M18 0 L36 36 L0 36 Z" />
                 </svg>
               </div>
               <div className="arrow" onClick={() => updateDirections(1)}>
-                <svg height="36" width="36" fill="#434343">
+                <svg height="36" width="36" fill="rgb(24, 24, 24)">
                   <path d="M0 0 L36 18 L0 36 Z" />
                 </svg>
               </div>
               <div className="arrow" onClick={() => updateDirections(2)}>
-                <svg height="36" width="36" fill="#434343">
+                <svg height="36" width="36" fill="rgb(24, 24, 24)">
                   <path d="M0 0 L36 0 L18 36 Z" />
                 </svg>
               </div>
               <div className="arrow" onClick={() => updateDirections(3)}>
-                <svg height="36" width="36" fill="#434343">
+                <svg height="36" width="36" fill="rgb(24, 24, 24)">
                   <path d="M0 18 L36 0 L36 36 Z" />
                 </svg>
               </div>
